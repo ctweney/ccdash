184b31717ad235db19825abffdbff0c33c121901
Chris Tweney <chris@media.berkeley.edu>
2014-03-14 13:27:54 -0700

    Add stats for HotPlate and LiveUpdates (currently pointed at localhost)

---
+++ server/stats.js
@@ -0,0 +1,37 @@
+var request = require('request');
+var sharedEvents = require('./sharedEventEmitter.js');
+
+var sendEvent = function(scrapeResponse) {
+  sharedEvents.emit('scraped.stats', scrapeResponse);
+};
+
+var parseStats = function(stats) {
+  stats = JSON.parse(stats);
+  stats.hot_plate.average_time = (stats.hot_plate.total_warmup_time / stats.hot_plate.total_warmups_requested).toFixed(1);
+  stats.hot_plate.outstanding_warmups = stats.hot_plate.total_warmups_requested - stats.hot_plate.total_warmups_processed;
+  stats.live_updates_warmer.average_time = (stats.live_updates_warmer.total_warmup_time / stats.live_updates_warmer.total_warmups_requested).toFixed(1);
+  sendEvent(stats);
+};
+
+var scrapeStats = function() {
+  request({
+    url: 'http://localhost:3000/api/stats',
+    rejectUnauthorized: false
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      process.nextTick(function() {
+        parseStats(body);
+      });
+    }
+  });
+};
+
+var init = function() {
+  setInterval(scrapeStats, 4000);
+};
+
+module.exports = {
+  init: init
+};
+
+
