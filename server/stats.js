c608f533bfddf20c3b606be4bf9e3103425e23d6
Chris Tweney <chris@media.berkeley.edu>
2014-03-20 08:26:04 -0700

    Add a warning state color, and make HotPlate stats and LiveUpdate stats use it

--- server/stats.js
+++ server/stats.js
@@ -7,9 +7,26 @@ var sendEvent = function(scrapeResponse) {
 
 var parseStats = function(stats) {
   stats = JSON.parse(stats);
-  stats.hot_plate.average_time = (stats.hot_plate.total_warmup_time / stats.hot_plate.total_warmups_requested).toFixed(1);
-  stats.hot_plate.outstanding_warmups = stats.hot_plate.total_warmups_requested - stats.hot_plate.total_warmups_processed;
-  stats.live_updates_warmer.average_time = (stats.live_updates_warmer.total_warmup_time / stats.live_updates_warmer.total_warmups_requested).toFixed(1);
+
+  // hot plate
+  stats.hot_plate.average_time = (stats.hot_plate.total_warmup_time / stats.hot_plate.total_warmups_processed).toFixed(1);
+  outstanding = stats.hot_plate.total_warmups_requested - stats.hot_plate.total_warmups_processed;
+  if (outstanding) {
+    stats.hot_plate.outstanding_warmups = outstanding;
+    stats.hot_plate.state = "ccdash-state-warning";
+  } else {
+    stats.hot_plate.state = "ccdash-state-successful";
+  }
+
+  // live updates
+  averageTime = (stats.live_updates_warmer.total_warmup_time / stats.live_updates_warmer.total_warmups_requested).toFixed(1);
+  stats.live_updates_warmer.average_time = averageTime;
+  if (averageTime > 5) {
+    stats.live_updates_warmer.state = "ccdash-state-warning";
+  } else {
+    stats.live_updates_warmer.state = "ccdash-state-successful";
+  }
+
   sendEvent(stats);
 };
 
